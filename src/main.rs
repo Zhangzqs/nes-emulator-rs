edd02f7f88a620836108019f98c59474230dc32c
Zhangzqs <i@zhangzqs.cn>
2022-03-18 11:04:13 +0800

    update: 分离代码，新增操作数地址获取函数

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,11 @@
+use crate::bus::Addressable;
+use crate::register::Register;
+use crate::status::StatusFlagRegister;
+use crate::AddressingMode::{ZeroPage, ZeroPageX};
+
 mod bus;
-
-/// # Status Register (P) http://wiki.nesdev.com/w/index.php/Status_flags
-///
-///  7 6 5 4 3 2 1 0
-///  N V _ B D I Z C
-///  | |   | | | | +--- Carry Flag
-///  | |   | | | +----- Zero Flag
-///  | |   | | +------- Interrupt Disable
-///  | |   | +--------- Decimal Mode (not used on NES)
-///  | |   +----------- Break Command
-///  | +--------------- Overflow Flag
-///  +----------------- Negative Flag
-pub struct StatusFlagRegister{
-    /// 进位标志
-    pub carry: bool,
-    /// 零标志
-    pub zero: bool,
-    /// 禁用中断
-    pub interrupt_disable: bool,
-    /// 十进制模式(nes中不使用)
-    pub decimal_mode: bool,
-    ///
-    pub break_command: bool,
-    /// 溢出标志
-    pub overflow: bool,
-    /// 负数标志
-    pub negative: bool,
-}
-
-pub struct Register{
-    /// 寄存器A
-    pub a: u8,
-    /// 寄存器X
-    pub x: u8,
-    /// 寄存器Y
-    pub y: u8,
-    /// 标志位寄存器
-    pub status: StatusFlagRegister,
-    /// 程序计数器(program counter)
-    pub pc: u16,
-    /// 栈指针寄存器(stack pointer)
-    pub sp: u8,
-}
+mod register;
+mod status;
 
 pub enum AddressingMode {
     /// 立即数寻址(操作码，操作数)
@@ -68,6 +32,66 @@ pub enum AddressingMode {
     NoneAddressing,
 }
 
+pub struct CPU {
+    pub register: Register,
+    pub bus: Box<dyn Addressable>,
+}
+
+impl CPU {
+    /// 获取当前的操作数地址
+    fn get_operand_address(&self, mode: &AddressingMode) -> u16 {
+        let pc = self.register.pc;
+        let x = self.register.x;
+        let y = self.register.y;
+
+        let read = |addr: u16| self.bus.read(addr);
+        let read_u16 = |addr: u16| self.bus.read_u16(addr);
+        /// 此时寄存器pc的值为指令码地址的后一个地址
+        match mode {
+            AddressingMode::Immediate => pc,
+
+            AddressingMode::ZeroPage => read(pc) as u16,
+            AddressingMode::Absolute => read_u16(pc),
+
+            AddressingMode::ZeroPageX
+            | AddressingMode::ZeroPageY
+            | AddressingMode::AbsoluteX
+            | AddressingMode::AbsoluteY => {
+                // 零页基地址
+                let base = match mode {
+                    AddressingMode::ZeroPageX | AddressingMode::ZeroPageY => read(pc) as u16,
+                    AddressingMode::AbsoluteX | AddressingMode::AbsoluteY => read_u16(pc),
+                    _ => todo!(),
+                };
+                let x_or_y = match mode {
+                    AddressingMode::ZeroPageX | AddressingMode::AbsoluteX => x,
+                    AddressingMode::ZeroPageY | AddressingMode::AbsoluteY => y,
+                    _ => todo!(),
+                };
+                base.wrapping_add(x_or_y as u16)
+            }
+            AddressingMode::IndirectX => {
+                /// X间接寻址(操作码，零页基地址)
+                /// *(X+base) | *(X+base+1) << 8
+                let base = read(pc);
+                let ptr = base.wrapping_add(x);
+                let low = read(ptr as u16);
+                let high = read(ptr.wrapping_add(1) as u16);
+                (low as u16) | ((high as u16) << 8)
+            }
+            AddressingMode::IndirectY => {
+                /// Y间接寻址(操作码，零页间接地址)
+                /// *(base) | *(base+1) << 8 + Y
+                let base_ptr = read(pc);
+                let low = read(base_ptr as u16);
+                let high = read(base_ptr.wrapping_add(1) as u16);
+                let base = (low as u16) | ((high as u16) << 8);
+                base.wrapping_add(y as u16)
+            }
+            AddressingMode::NoneAddressing => panic!("mode {:?} is not supported", mode),
+        }
+    }
+}
 
 fn main() {
     println!("Hello, world!");
