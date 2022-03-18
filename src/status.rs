606a63294f5d72f7869b63cd0e63acc5af7f014d
Zhangzqs <i@zhangzqs.cn>
2022-03-18 17:29:19 +0800

    add: 添加状态寄存器到u8的相互转化

--- src/status.rs
+++ src/status.rs
@@ -32,6 +32,40 @@ pub struct StatusFlagRegister {
     pub negative: bool,
 }
 
+impl From<u8> for StatusFlagRegister {
+    fn from(bits: u8) -> Self {
+        Self {
+            carry: ((bits >> 0) & 1) != 0,
+            zero: ((bits >> 1) & 1) != 0,
+            interrupt_disable: ((bits >> 2) & 1) != 0,
+            decimal_mode: ((bits >> 1) & 3) != 0,
+            break_command: ((bits >> 1) & 4) != 0,
+            unused: ((bits >> 1) & 5) != 0,
+            overflow: ((bits >> 1) & 6) != 0,
+            negative: ((bits >> 1) & 7) != 0,
+        }
+    }
+}
+
+impl Into<u8> for StatusFlagRegister {
+    fn into(self) -> u8 {
+        let bit0 = self.carry as u8;
+        let bit1 = self.zero as u8;
+        let bit2 = self.interrupt_disable as u8;
+        let bit3 = self.decimal_mode as u8;
+        let bit4 = self.break_command as u8;
+        let bit5 = self.unused as u8;
+        let bit6 = self.overflow as u8;
+        let bit7 = self.negative as u8;
+        let bits = [bit0, bit1, bit2, bit3, bit4, bit5, bit6, bit7];
+        let mut result: u8 = 0;
+        for (i, bit) in bits.iter().enumerate() {
+            result = result | (bit << i);
+        }
+        result
+    }
+}
+
 impl Default for StatusFlagRegister {
     fn default() -> Self {
         Self {
