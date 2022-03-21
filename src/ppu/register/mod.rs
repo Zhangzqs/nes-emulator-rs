ae04eca7c52f7f8540ebc513dc51b88725372f7f
Zhangzqs <i@zhangzqs.cn>
2022-03-21 17:48:49 +0800

    add: 新增ppu寄存器结构体

--- src/ppu/register/mod.rs
+++ src/ppu/register/mod.rs
@@ -1,5 +1,30 @@
+use self::{
+    address::AddressRegister, control::ControlRegister, mask::MaskRegister, scroll::ScrollRegister,
+    status::StatusRegister,
+};
+
 pub mod address;
 pub mod control;
 pub mod mask;
 pub mod scroll;
 pub mod status;
+
+pub struct PpuRegister {
+    pub control: ControlRegister,
+    pub mask: MaskRegister,
+    pub status: StatusRegister,
+    pub scroll: ScrollRegister,
+    pub address: AddressRegister,
+}
+
+impl PpuRegister {
+    pub fn new() -> Self {
+        Self {
+            control: ControlRegister::new(),
+            mask: MaskRegister::new(),
+            status: StatusRegister::new(),
+            scroll: ScrollRegister::new(),
+            address: AddressRegister::new(),
+        }
+    }
+}
