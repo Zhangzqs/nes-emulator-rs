20114f571beb3e9d0c85a774d0a8bdcbec77cd82
Zhangzqs <i@zhangzqs.cn>
2022-03-21 19:50:41 +0800

    add: 新增ppu寻址方法

--- src/addressable.rs
+++ src/addressable.rs
@@ -14,3 +14,20 @@ pub trait Addressable {
         self.write(addr + 1, high);
     }
 }
+
+pub trait MutableAddressable {
+    fn read(&mut self, addr: u16) -> u8;
+    fn write(&mut self, addr: u16, data: u8);
+
+    fn read_u16(&mut self, addr: u16) -> u16 {
+        let low = self.read(addr) as u16;
+        let high = self.read(addr + 1) as u16;
+        (high << 8) | low
+    }
+    fn write_u16(&mut self, addr: u16, data: u16) {
+        let low = (data & 0xFF) as u8;
+        let high = (data >> 8) as u8;
+        self.write(addr, low);
+        self.write(addr + 1, high);
+    }
+}
--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -1,4 +1,8 @@
-use crate::{addressable::Addressable, meta::Mirror};
+use crate::{
+    addressable::{Addressable, MutableAddressable},
+    flag::FlagRegister,
+    meta::Mirror,
+};
 
 use self::register::PpuRegister;
 mod register;
@@ -39,12 +43,156 @@ impl Ppu {
     }
 }
 
-impl Addressable for Ppu {
-    fn read(&self, addr: u16) -> u8 {
-        todo!()
+impl Ppu {
+    // Horizontal:
+    //   [ A ] [ a ]
+    //   [ B ] [ b ]
+
+    // Vertical:
+    //   [ A ] [ B ]
+    //   [ a ] [ b ]
+    pub fn mirror_vram_addr(&self, addr: u16) -> u16 {
+        let mirrored_vram = addr & 0b10111111111111; // mirror down 0x3000-0x3eff to 0x2000 - 0x2eff
+        let vram_index = mirrored_vram - 0x2000; // to vram vector
+        let name_table = vram_index / 0x400;
+        match (&self.mirror, name_table) {
+            (Mirror::Vertical, 2) | (Mirror::Vertical, 3) => vram_index - 0x800,
+            (Mirror::Horizontal, 2) => vram_index - 0x400,
+            (Mirror::Horizontal, 1) => vram_index - 0x400,
+            (Mirror::Horizontal, 3) => vram_index - 0x800,
+            _ => vram_index,
+        }
+    }
+
+    fn increment_vram_addr(&mut self) {
+        self.register
+            .address
+            .increment(self.register.control.vram_address_increment());
+    }
+
+    fn write_to_ctrl(&mut self, value: u8) {
+        let before_nmi_status = self.register.control.generate_vblank_nmi();
+        self.register.control.update(value);
+    }
+
+    fn write_to_mask(&mut self, value: u8) {
+        self.register.mask.update(value);
+    }
+
+    fn read_status(&mut self) -> u8 {
+        let data = self.register.status.snapshot();
+        self.register.status.reset_vblank_status();
+        self.register.address.reset_latch();
+        self.register.scroll.reset_latch();
+        data
+    }
+
+    fn write_to_oam_addr(&mut self, value: u8) {
+        self.oam_address = value;
+    }
+
+    fn write_to_oam_data(&mut self, value: u8) {
+        self.oam_data[self.oam_address as usize] = value;
+        self.oam_address = self.oam_address.wrapping_add(1);
+    }
+
+    fn read_oam_data(&self) -> u8 {
+        self.oam_data[self.oam_address as usize]
+    }
+
+    fn write_to_scroll(&mut self, value: u8) {
+        self.register.scroll.write(value);
+    }
+
+    fn write_to_ppu_addr(&mut self, value: u8) {
+        self.register.address.update(value);
+    }
+
+    fn write_to_data(&mut self, value: u8) {
+        let addr = self.register.address.get();
+        match addr {
+            0..=0x1fff => println!("attempt to write to chr rom space {}", addr),
+            0x2000..=0x2fff => {
+                self.vram[self.mirror_vram_addr(addr) as usize] = value;
+            }
+            0x3000..=0x3eff => unimplemented!("addr {} shouldn't be used in reallity", addr),
+
+            //Addresses $3F10/$3F14/$3F18/$3F1C are mirrors of $3F00/$3F04/$3F08/$3F0C
+            0x3f10 | 0x3f14 | 0x3f18 | 0x3f1c => {
+                let add_mirror = addr - 0x10;
+                self.palette_table[(add_mirror - 0x3f00) as usize] = value;
+            }
+            0x3f00..=0x3fff => {
+                self.palette_table[(addr - 0x3f00) as usize] = value;
+            }
+            _ => panic!("unexpected access to mirrored space {}", addr),
+        }
+        self.increment_vram_addr();
+    }
+
+    fn read_data(&mut self) -> u8 {
+        let addr = self.register.address.get();
+
+        self.increment_vram_addr();
+
+        match addr {
+            0..=0x1fff => {
+                let result = self.internal_data_buffer;
+                self.internal_data_buffer = self.chr_rom[addr as usize];
+                result
+            }
+            0x2000..=0x2fff => {
+                let result = self.internal_data_buffer;
+                self.internal_data_buffer = self.vram[self.mirror_vram_addr(addr) as usize];
+                result
+            }
+            0x3000..=0x3eff => unimplemented!("addr {} shouldn't be used in reallity", addr),
+
+            //Addresses $3F10/$3F14/$3F18/$3F1C are mirrors of $3F00/$3F04/$3F08/$3F0C
+            0x3f10 | 0x3f14 | 0x3f18 | 0x3f1c => {
+                let add_mirror = addr - 0x10;
+                self.palette_table[(add_mirror - 0x3f00) as usize]
+            }
+
+            0x3f00..=0x3fff => self.palette_table[(addr - 0x3f00) as usize],
+            _ => panic!("unexpected access to mirrored space {}", addr),
+        }
+    }
+
+    fn write_oam_dma(&mut self, data: &[u8; 256]) {
+        for x in data.iter() {
+            self.oam_data[self.oam_address as usize] = *x;
+            self.oam_address = self.oam_address.wrapping_add(1);
+        }
+    }
+}
+
+/// cpu通过总线内存访问与ppu通信，共暴露8个字节的寄存器
+impl MutableAddressable for Ppu {
+    fn read(&mut self, addr: u16) -> u8 {
+        match addr {
+            0 | 1 | 3 | 5 | 6 => {
+                panic!("Attempt to read from write-only PPU address {}", addr);
+                // 0
+            }
+            2 => self.read_status(),
+            4 => self.read_oam_data(),
+            7 => self.read_data(),
+            _ => panic!("Can't read ppu register: {}", addr),
+        }
     }
 
     fn write(&mut self, addr: u16, data: u8) {
-        todo!()
+        match addr {
+            0 => self.write_to_ctrl(data),
+            1 => self.write_to_mask(data),
+            2 => panic!("attempt to write to PPU status register"),
+            3 => self.write_to_oam_addr(data),
+            4 => self.write_to_oam_data(data),
+            5 => self.write_to_scroll(data),
+            6 => self.write_to_ppu_addr(data),
+            7 => self.write_to_data(data),
+            _ => panic!("Can't write ppu register: {}, data: {}", addr, data),
+        }
     }
 }
